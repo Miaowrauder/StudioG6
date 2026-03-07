2e93032ba064a52c5f32b0c3355911b2f83850c6
moscitow9879 <moscitow9879@users.noreply.github.com>
2026-03-07 21:49:17 +0000

    Spawn Points
    Added random selection of spawn point

--- CupidsEnvoyProj/Assets/Scripts/Player.cs
+++ CupidsEnvoyProj/Assets/Scripts/Player.cs
@@ -1,8 +1,4 @@
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
--- CupidsEnvoyProj/Assets/Scripts/Spawner.cs
+++ CupidsEnvoyProj/Assets/Scripts/Spawner.cs
@@ -1,20 +1,23 @@
-using System.Threading;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject[] zombieTypes;
+    private GameObject[] spawnPoints;
     public float spawnTime = 3;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawnPoints = GameObject.FindGameObjectsWithTag("Spawn Point");
         InvokeRepeating("SpawnZombie", 0, spawnTime);
     }
 
     // Update is called once per frame
     private void SpawnZombie()
     {
-        Instantiate(zombieTypes[Random.Range(0, zombieTypes.Length)], transform.position, transform.rotation);
+        int spawnAt = Random.Range(0, spawnPoints.Length);
+        print(spawnAt);
+        Instantiate(zombieTypes[Random.Range(0, zombieTypes.Length)], spawnPoints[spawnAt].transform.position, spawnPoints[spawnAt].transform.rotation);
     } 
 }
