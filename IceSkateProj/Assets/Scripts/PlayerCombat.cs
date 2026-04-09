b53de8a5cce39aa75b7b19b69814dc92c4ca2bd8
MarauderFive <7ewilkinson7@gmail.com>
2026-04-09 20:52:04 +0100

    v.0.2.4
    Added teapot attack, restructured combo script, small adjustments to combat and movement system

--- IceSkateProj/Assets/Scripts/PlayerCombat.cs
+++ IceSkateProj/Assets/Scripts/PlayerCombat.cs
@@ -1,9 +1,13 @@
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayerCombat : MonoBehaviour
 {
     private Animator animator;
+    public playerCombo plCombo;
+    private playerMovement plMove;
     public int health;
     [Header("Damage Attack")]
     public int strength;
@@ -11,10 +15,18 @@ public class PlayerCombat : MonoBehaviour
     [Header("Push Attack")]
     public int power;
     public float pushRange;
+    [Header("Teapy Attack")]
+    public float damageRadius, comboSpendDelay;
+    public bool teapyActive;
+    private bool canLoop;
+    public GameObject teapyPos;
+    public GameObject teapyPrefab;
+    private GameObject teapotSpawned;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        plMove = GetComponent<playerMovement>();
     }
     
     void Update()
@@ -24,6 +36,53 @@ public class PlayerCombat : MonoBehaviour
             //animator.SetTrigger("Damage_Attack"); //Decide binds later
             animator.SetTrigger("Push_Attack");
         }
+
+        if(Input.GetKeyDown(KeyCode.Mouse2)) //set animator logic and proper binds later
+        {
+            if((plCombo.comboCount >= 1) && !teapyActive && (plMove.movement.magnitude != 0f))//and animator is free
+            {
+                teapotSpawned = Instantiate(teapyPrefab, teapyPos.transform.position, Quaternion.identity);
+                teapotSpawned.transform.SetParent(this.gameObject.transform);
+                teapyActive = true;
+                canLoop = true;
+            }
+            
+            //set anim state
+        }
+
+        if((Input.GetKeyUp(KeyCode.Mouse2)) || (plCombo.comboCount == 0) || (plMove.movement.magnitude == 0f)) //end teapy time
+        {
+            teapyActive = false;
+            Destroy(teapotSpawned);
+            //set anim state!
+        }
+
+        if(teapyActive && canLoop)
+        {
+            canLoop = false;
+            StartCoroutine(TeapyTick());
+        }
+    }
+
+    IEnumerator TeapyTick()
+    {
+        plCombo.ComboSpend(-1);
+        yield return new WaitForSeconds(comboSpendDelay);
+        canLoop = true;
+    }
+
+    void FixedUpdate()
+    {
+        if(teapyActive)
+        {
+            TeapyPosUpdate();
+        }
+    }
+
+    private void TeapyPosUpdate()
+    {
+        Vector3 teapyVector = (plMove.movement * 2);
+        teapotSpawned.transform.position = transform.position + teapyVector;
     }
 
     public void TakeDamage(int damage)
