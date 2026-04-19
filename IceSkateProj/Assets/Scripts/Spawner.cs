dbc35b44c4311e6d341b4f80c385f91783bffa45
MarauderFive <7ewilkinson7@gmail.com>
2026-04-19 21:45:59 +0100

    V1.0.1
    Fixes and adjustments - vfx emitter empties are now destroyed on wave end, adjusted visuals for push to better indicate range, reduced splash particles to improve performance, altered snowball prefab to fix detonation issues, added temporary bucket enemy, added variable enemy move speed to reduce grouping somewhat, adjusted ice regerneration to every 3 waves, fixed fatal spawner crashes

--- IceSkateProj/Assets/Scripts/Spawner.cs
+++ IceSkateProj/Assets/Scripts/Spawner.cs
@@ -9,6 +9,7 @@ public class Spawner : MonoBehaviour
     public AnimationCurve spawnTime;
     public float waveStartDelay;
     private int spawnPoints, enemyTotal;
+    private int emergencyBreak;
 
 
     void Start()
@@ -27,11 +28,12 @@ public class Spawner : MonoBehaviour
     IEnumerator SpawnEnemy()
     {
         enemyTotal = 0;
+        emergencyBreak = 0;
 
         yield return new WaitForSeconds(waveStartDelay);
-        while (spawnPoints >= 10) // Set to lowest spawn cost
+        while ((spawnPoints >= 10) && (emergencyBreak < 10)) // Set to lowest spawn cost
         {
-            if (enemyTypes.Length == 0)
+            if ((enemyTypes.Length == 0) || (emergencyBreak > 10))
             {
                 print("No valid enemies");
                 break;
@@ -40,25 +42,39 @@ public class Spawner : MonoBehaviour
             int spawnAt = Random.Range(0, spawnLocations.Length);
             int enemySelected = -1;
 
-            while (enemySelected == -1)
+            while ((enemySelected == -1) && (emergencyBreak < 10))
             {
                 enemySelected = Random.Range(0, enemyTypes.Length);
                 int spawnCost = enemyTypes[enemySelected].GetComponent<Enemy>().spawnCost;
-                int waveEnabled = enemyTypes[enemySelected].GetComponent<Enemy>().waveEnabled;
+                int waveEnabled = enemyTypes[enemySelected].GetComponent<Enemy>().waveEnabled; //i know theres like 10 break clauses here but it would NOT stop crashing
                 int currentWave = gameManager.waveNumber;
 
                 if (spawnPoints - spawnCost < 0 || waveEnabled > currentWave)
                 {
                     enemySelected = -1;
+                    emergencyBreak++;
                 }
                 else
                 {
                     spawnPoints -= spawnCost;
                 }
+
+                if(emergencyBreak > 10)
+                {
+                    print("No valid enemies");
+                    break;
+                }
             }
 
-            Instantiate(enemyTypes[enemySelected], spawnLocations[spawnAt].transform.position, spawnLocations[spawnAt].transform.rotation);
-            gameManager.currentEnemies++;
+            if(emergencyBreak < 10)
+            {
+                Instantiate(enemyTypes[enemySelected], spawnLocations[spawnAt].transform.position, spawnLocations[spawnAt].transform.rotation);
+                gameManager.currentEnemies++;
+            }
+            else
+            {
+                print("Spawn Failed");
+            }
 
             yield return new WaitForSeconds(spawnTime.Evaluate(enemyTotal));
         }
